bbdbd05701b8e320395f112c030035652b1fb1a5
adrianoforcellini <adrianoforcellinipedretti@gmail.com>
2025-06-28 21:16:12 -0300

    Criação da rota PUT

--- products/productsController.js
+++ products/productsController.js
@@ -12,8 +12,8 @@ router.get("/", async (req, res) => {
 });
 
 router.post(`/${process.env.POST_ROUTE}`, async (req, res) => {
-    const { name, description, price, img_url } = req.body;
-    const product = { name, description, price, img_url }
+    const { name, description, price, img_url, stock } = req.body;
+    const product = { name, description, price, img_url, stock }
 
     try {
         await Products.create(product);
@@ -23,4 +23,26 @@ router.post(`/${process.env.POST_ROUTE}`, async (req, res) => {
     }
 });
 
+router.put(`/${process.env.PUT_ROUTE}/:id`, async (req, res) => {
+    const id = req.params.id;
+    const { name, description, price, img_url, stock } = req.body;
+    const product = { name, description, price, img_url, stock }
+    console.log(id)
+
+    try {
+        const updateProduct = await Products.updateOne({ _id: id }, product);
+        if (updateProduct.modifiedCount === 1) {
+            res
+                .status(200)
+                .json({ message: "Sucesso. Produto atualizado", product: product });
+        }
+        res
+            .status(400)
+            .json({
+                message: "Houve uma falha. Verifique se de fato você modificou os valores de todos os campos: name, description, price, img_url e stock ",
+            });
+    } catch (error) {
+        res.status(500).json({ error });
+    }
+});
 module.exports = router;
